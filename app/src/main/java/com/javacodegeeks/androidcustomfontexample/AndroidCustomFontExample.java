f04e408c985995e37eac802e495203a1d2623f95
arpit999bicubic <androi.bicubics@gmail.com>
2016-12-02 16:05:28 +0530

    Initial commit

---
+++ app/src/main/java/com/javacodegeeks/androidcustomfontexample/AndroidCustomFontExample.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks.androidcustomfontexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidCustomFontExample extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
